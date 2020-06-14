47646a24d9dc3a802a633d94ec0d9805352250c6
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-06-14 17:38:32 -0400

    added shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -23,17 +23,22 @@ var fight = function(enemyName) {
 
         // if user picks "skip", confirm and then stop the loop
         if (promptFight === "skip" || promptFight === "SKIP") {
-            // confirm user wants to skip
-            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+            if (playerMoney >= 10) {
+                // confirm user wants to skip
+                var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-            // if yes (true), leave fight
-            if (confirmSkip) {
-                window.alert(playerName + " has decided to skip this fight. Goodbye!");
-                // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
-                console.log("playerMoney", playerMoney);
-                break;
-            } 
+                // if yes (true), leave fight
+                if (confirmSkip) {
+                    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                    // subtract money from playerMoney for skipping
+                    playerMoney = playerMoney - 10;
+                    console.log("playerMoney", playerMoney);
+                    break;
+                }
+            }
+            else {
+                window.alert("You don't have enough money!");
+            }
         }
         
         // reduce enemyHealth by subtracting the amount set in the playerAttack variable
@@ -94,10 +99,21 @@ var startGame = function () {
             enemyHealth = 50;
 
             // use debugger to pause script from running and check what's going on at that moment in the code
-            debugger;
+            // debugger;
 
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
+
+            // if player is still alive and we're not at the last enemy in the array
+                if (playerHealth > 0 && i < enemyNames.length - 1) {
+                    // ask if user wants to use the store before the next round
+                    var storeConfirm = window.confirm("The fight is over. Visit the shop before the next round?");
+                    
+                    // if yes, take them to the shop() function
+                    if (storeConfirm) {
+                        shop();
+                    }
+                }
         } else {
             window.alert("You have lost your robot in battle! Game Over!");
             break;
@@ -127,6 +143,41 @@ var endGame = function() {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 };
+
+var shop = function() {
+    // ask player what they'd like to do
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store. Please enter one: REFILL, UPGRADE, or LEAVE to make a choice."
+    );
+    switch (shopOptionPrompt) {
+        case "REFILL":
+        case "refill":
+            window.alert("Refilling player's health by 20 for 7 dollars.");
+            //increase health and decrease money
+            playerHealth = playerHealth + 20;
+            playerMoney = playerMoney - 7;
+            console.log("Health: " + playerHealth + ", Money: " + playerMoney);
+            break;
+        case "UPGRADE":
+        case "upgrade":
+            window.alert("Upgrading player's attack by 6 for 7 dollars.");
+            //increase attack and decrease money
+            playerAttack = playerAttack + 6;
+            playerMoney = playerMoney - 7;
+            console.log("Attack: " + playerAttack + ", Money: " + playerMoney);
+            break;
+        case "LEAVE":
+        case "leave":
+            window.alert("Leaving the store.")
+            break;
+        default:
+            window.alert("Please enter a valid option.")
+            // call shop() again to force player to pick a valid option
+            shop();
+            break;
+    }
+
+};  
 
 // start the game when the page loads
 startGame();
