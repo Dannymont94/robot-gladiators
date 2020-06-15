4f9cea4b59434e642fb7ecd5618a8748d4d90638
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-06-15 14:56:21 -0400

    fixed bug that allowed playerInfo.name to be undefined or null

--- assets/js/game.js
+++ assets/js/game.js
@@ -167,9 +167,20 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name ="";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+}
+
 // create playerInfo object
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -200,8 +211,6 @@ var playerInfo = {
         }
     }
 };
-
-console.log(playerInfo.name);
 
 // create enemyInfo object
 var enemyInfo = [
