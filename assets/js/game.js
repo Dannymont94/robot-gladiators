d80e0a351164a34fbd3695352ab06bc86f205723
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-06-15 22:38:56 -0400

    shop function now accepts integer values

--- assets/js/game.js
+++ assets/js/game.js
@@ -120,19 +120,18 @@ var endGame = function() {
 var shop = function() {
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store. Please enter one: REFILL, UPGRADE, or LEAVE to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
+    // convert to integer
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.")
             break;
         default:
@@ -164,7 +163,7 @@ var getPlayerName = function() {
 
 var fightOrSkip = function() {
     // ask user whether to fight or skip
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");    
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");    
 
     if (promptFight === "" || promptFight === null) {
         window.alert("You need to provide a valid answer! Please try again.");
